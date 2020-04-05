67f38cca9465908e41e9772d2a731dbc90057744
Bryce Koehl <bryce.koehl@iga.in.gov>
2020-04-05 13:52:12 -0400

    Finished the Index Page.

--- Lab08/index.php
+++ Lab08/index.php
@@ -16,3 +16,28 @@ require_once ("vendor/autoload.php");
 $user_controller = new UserController();
 
 //add your code below this line to complete this file
+
+//Test and retrieve the action from a querystring variable named actions
+if (!($action = filter_input(INPUT_GET,"action", FILTER_SANITIZE_STRING))){
+    $action = "index";
+}
+
+//depending on the action value, a different method will need to be called
+switch ($action) {
+    case "index": $user_controller->index();
+        break;
+    case "register": $user_controller->register();
+        break;
+    case "login": $user_controller->login();
+        break;
+    case "verify": $user_controller->verify();
+        break;
+    case "logout": $user_controller->logout();
+        break;
+    case "reset": $user_controller->reset();
+        break;
+    case "execute_reset": $user_controller->execute_reset();
+        break;
+    default : $user_controller->error("Invalid action was requested");
+        break;
+};
