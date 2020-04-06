24c1183931bcd070c9496e1a63c2512072c01467
nguyenash <nguyen.ash1@gmail.com>
2020-04-06 19:59:33 -0400

    working on login view page

--- Lab08/views/login/login.class.php
+++ Lab08/views/login/login.class.php
@@ -8,7 +8,7 @@
 
 class Login extends View
 {
-  public function display($message){
+  public function display(){
       parent::header();
       ?>
 
@@ -18,13 +18,14 @@ class Login extends View
 
       <!-- middle row -->
       <div class="middle-row">
-          <h3>Please log in.</h3>
-          <p><?= $message ?></p>
+          <form action = "index.php?action=login"  method = "POST">
+              <div> <input type="text" name="username" placeholder="Username" required style='width:99%'/></div>
+              <div> <input type="password" name="password" placeholder="Password, 5 characters minimum" required minlength = "5" style = 'width:99%'/> </div>
+          </form>
       </div>
 
       <!-- bottom row for links  -->
       <div class="bottom-row">
-          <span style="float: left">Already have an account? <a href="index.php?action=login">Login</a></span>
           <span style="float: right">Don't have an account? <a href="index.php">Register</a></span>
       </div>
 
